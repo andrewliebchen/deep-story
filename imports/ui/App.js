25b7a8f68e30a8d6ff7fc4eb8d314815c7f5374d
Andrew Liebchen <liebchen@fb.com>
2020-11-10 13:57:38 -0800

    Set a max-width

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -9,44 +9,46 @@ export default (props) => {
   const stories = useTracker(() => StoriesCollection.find({}).fetch());
 
   return (
-    <Box p={3}>
-      <Flex
-        sx={{ mb: 3, alignItems: "center", justifyContent: "space-between" }}
-      >
-        <Heading>Deep Story</Heading>
-        <Button
-          onClick={() =>
-            setColorMode(colorMode === "default" ? "dark" : "default")
-          }
+    <Flex sx={{ justifyContent: "center" }}>
+      <Box sx={{ p: 3, maxWidth: 600 }}>
+        <Flex
+          sx={{ mb: 3, alignItems: "center", justifyContent: "space-between" }}
         >
-          {colorMode === "default" ? "Dark" : "Light"}
-        </Button>
-      </Flex>
-      <Flex sx={{ flexFlow: "wrap" }}>
-        {stories.length === 0 && <InlineInput />}
-        {stories.map((story, index) => {
-          const isLast = stories.length - 1 === index;
-          switch (story.type) {
-            case "break":
-              return (
-                <Flex
-                  key={story._id}
-                  sx={{ flexBasis: "100%", mb: 3, pt: isLast ? 2 : 0 }}
-                >
-                  {isLast && <InlineInput />}
-                </Flex>
-              );
-              break;
-            default:
-              return (
-                <Flex key={story._id} sx={{ flexShrink: 0 }}>
-                  <Text sx={{ mr: 1 }}>{story.text}</Text>
-                  {isLast && <InlineInput />}
-                </Flex>
-              );
-          }
-        })}
-      </Flex>
-    </Box>
+          <Heading>Deep Story</Heading>
+          <Button
+            onClick={() =>
+              setColorMode(colorMode === "default" ? "dark" : "default")
+            }
+          >
+            {colorMode === "default" ? "Dark" : "Light"}
+          </Button>
+        </Flex>
+        <Flex sx={{ flexFlow: "wrap" }}>
+          {stories.length === 0 && <InlineInput />}
+          {stories.map((story, index) => {
+            const isLast = stories.length - 1 === index;
+            switch (story.type) {
+              case "break":
+                return (
+                  <Flex
+                    key={story._id}
+                    sx={{ flexBasis: "100%", mb: 3, pt: isLast ? 2 : 0 }}
+                  >
+                    {isLast && <InlineInput />}
+                  </Flex>
+                );
+                break;
+              default:
+                return (
+                  <Flex key={story._id} sx={{ flexShrink: 0 }}>
+                    <Text sx={{ mr: 1 }}>{story.text}</Text>
+                    {isLast && <InlineInput />}
+                  </Flex>
+                );
+            }
+          })}
+        </Flex>
+      </Box>
+    </Flex>
   );
 };
