cc928ac179f5f62a7b3822603156baa5cb7454fd
Andrew Liebchen <liebchen@fb.com>
2020-11-25 20:57:29 -0800

    Re-create a text editor, lol

--- imports/ui/InlineInput.js
+++ imports/ui/InlineInput.js
@@ -1,4 +1,4 @@
-import { Input } from "theme-ui";
+import { Box, Flex, Input, Text } from "theme-ui";
 import { RefsCollection } from "../api/refs";
 import { useKeycodes } from "@accessible/use-keycode";
 import AppContext from "./AppContext";
@@ -34,12 +34,29 @@ const InlineInput = () => {
   });
 
   return (
-    <Input
-      autoFocus={true}
-      ref={ref}
-      value={value}
-      onChange={(event) => setValue(event.target.value.trim())}
-    />
+    <Flex>
+      <Input
+        autoFocus
+        ref={ref}
+        value={value}
+        onChange={(event) => setValue(event.target.value.trim())}
+        tabIndex={0}
+        onBlur={(event) => event.target.focus()}
+        sx={{
+          appearance: "none",
+          border: 0,
+          caretColor: "transparent",
+          p: 0,
+          width: 0,
+          overflow: "visible",
+          "&:focus": {
+            outline: "none",
+          },
+        }}
+      />
+      <Text variant="ref">{value}</Text>
+      <Box sx={{ bg: "primary", width: "0.5em" }} />
+    </Flex>
   );
 };
 
