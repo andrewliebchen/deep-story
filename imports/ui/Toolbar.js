b6011a8846fba6b4e47ab51ff7fdea4af84e02ef
Andrew Liebchen <liebchen@fb.com>
2020-12-01 12:26:01 -0800

    Stub in ref types, close button for selected refs

--- imports/ui/Toolbar.js
+++ imports/ui/Toolbar.js
@@ -1,18 +1,21 @@
-import React, { useContext } from "react";
 import { Box, Button, Flex, IconButton, Text } from "theme-ui";
-import { Link } from "react-router-dom";
-import { Edit2, X } from "react-feather";
-import AppContext from "./AppContext";
-import AccountToggle from "./AccountToggle";
-import ColorModeToggle from "./ColorModeToggle";
 import { isReady } from "../utils/helpers";
-import UilTrash from "@iconscout/react-unicons/icons/uil-trash";
+import { refTypes } from "../utils/types";
+import { Link } from "react-router-dom";
+import AccountToggle from "./AccountToggle";
+import AppContext from "./AppContext";
+import ColorModeToggle from "./ColorModeToggle";
+import React, { useContext } from "react";
 import UilArrowDown from "@iconscout/react-unicons/icons/uil-arrow-down";
 import UilArrowUp from "@iconscout/react-unicons/icons/uil-arrow-up";
 import UilFileBlank from "@iconscout/react-unicons/icons/uil-file-blank";
+import UilTrash from "@iconscout/react-unicons/icons/uil-trash";
+import UilTimes from "@iconscout/react-unicons/icons/uil-times";
 
 const Toolbar = () => {
-  const { selectedRefId, parentRef, userId } = useContext(AppContext);
+  const { selectedRefId, parentRef, userId, setSelectedRefId } = useContext(
+    AppContext
+  );
 
   return (
     <>
@@ -26,41 +29,58 @@ const Toolbar = () => {
           <Link to={`/r/${parentRef.parentId}`}>
             <Button variant="button.default">
               <UilArrowUp />
-              <Text ml={3}>{parentRef.text}</Text>
+              <Text ml={2}>{parentRef.text}</Text>
             </Button>
           </Link>
         )}
       </Flex>
 
-      {selectedRefId && (
-        <Flex
-          sx={{
-            alignItems: "center",
-            position: "fixed",
-            top: 2,
-            left: "50%",
-            transform: "translateX(-50%)",
-            zIndex: 1,
-          }}
-        >
-          <IconButton
-            variant="iconButton.negative"
-            mr={2}
-            onClick={() =>
-              window.confirm("You sure you want to delete this?") &&
-              Meteor.call("refs.remove", selectedRefId)
-            }
-          >
-            <UilTrash />
-          </IconButton>
-          <Link to={`/r/${selectedRefId}`}>
-            <Button variant="button.primary">
-              <UilArrowDown />
-              <Text ml={3}>Dive</Text>
-            </Button>
-          </Link>
-        </Flex>
-      )}
+      <Flex
+        sx={{
+          alignItems: "center",
+          position: "fixed",
+          top: 2,
+          left: "50%",
+          transform: "translateX(-50%)",
+          zIndex: 1,
+        }}
+      >
+        {selectedRefId ? (
+          <>
+            <IconButton
+              variant="iconButton.default"
+              children={<UilTimes />}
+              onClick={() => setSelectedRefId("")}
+            />
+            <IconButton
+              sx={{ mx: 2, variant: "iconButton.negative" }}
+              children={<UilTrash />}
+              onClick={() =>
+                window.confirm("You sure you want to delete this?") &&
+                Meteor.call("refs.remove", selectedRefId)
+              }
+            />
+            <Link to={`/r/${selectedRefId}`}>
+              <Button variant="button.primary">
+                <UilArrowDown />
+                <Text ml={2}>Dive</Text>
+              </Button>
+            </Link>
+          </>
+        ) : (
+          <>
+            {Object.keys(refTypes).map((type) => (
+              <IconButton
+                key={type}
+                children={refTypes[type].icon}
+                title={type}
+                sx={{ mx: 1, variant: "iconButton.default" }}
+              />
+            ))}
+          </>
+        )}
+      </Flex>
+
       <Flex
         ml="auto"
         sx={{
--- imports/utils/types.js
+++ imports/utils/types.js
@@ -1,1 +1,29 @@
-export const storyTypes = ["prose", "mock", "table", "user"];
+import React from "react";
+import UilImage from "@iconscout/react-unicons/icons/uil-image";
+import UilLinkH from "@iconscout/react-unicons/icons/uil-link-h";
+import UilSmileBeam from "@iconscout/react-unicons/icons/uil-smile-beam";
+import UilTable from "@iconscout/react-unicons/icons/uil-table";
+import UilTextFields from "@iconscout/react-unicons/icons/uil-text-fields";
+
+export const refTypes = {
+  story: {
+    icon: <UilTextFields />,
+    label: "Story",
+  },
+  table: {
+    icon: <UilTable />,
+    label: "Table",
+  },
+  image: {
+    icon: <UilImage />,
+    label: "Image",
+  },
+  link: {
+    icon: <UilLinkH />,
+    label: "Link",
+  },
+  mock: {
+    icon: <UilSmileBeam />,
+    label: "Mock",
+  },
+};
