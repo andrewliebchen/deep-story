6109950297d14bf7629d6882bd497202e0c25a58
Andrew Liebchen <liebchen@fb.com>
2020-12-16 09:22:05 -0800

    New base ref button

--- imports/ui/BaseRefsList.js
+++ imports/ui/BaseRefsList.js
@@ -1,56 +1,37 @@
 import { Box, Button, Flex, Grid, Text } from "theme-ui";
 import { isReady } from "../utils/helpers";
-import { Link, useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useAccount, useBaseRefs } from "../utils/hooks";
-import AppContext from "./AppContext";
-import React, { useContext } from "react";
+import React from "react";
 import UilPlus from "@iconscout/react-unicons/icons/uil-plus";
 
 const BaseRefsList = () => {
-  const { getRefs } = useContext(AppContext);
-  const { userId } = useAccount();
   const { refs } = useBaseRefs();
-  const history = useHistory();
 
   return (
-    <>
-      <Button
-        variant="button.primary"
-        onClick={() =>
-          Meteor.call(
-            "refs.insert",
-            { type: "base", parentId: userId },
-            (error, id) => history.push(`/refs/${id}`)
-          )
-        }
-      >
-        <UilPlus />
-        <Text ml={2}>Create</Text>
-      </Button>
-      <Flex
+    <Flex
+      sx={{
+        alignItems: "center",
+        height: "100vh",
+        justifyContent: "center",
+        width: "100vw",
+      }}
+    >
+      <Grid
         sx={{
-          alignItems: "center",
-          height: "100vh",
-          justifyContent: "center",
-          width: "100vw",
+          gap: 0,
+          gridTemplateColumns: "repeat(3, 1fr)",
+          m: 3,
+          width: "container",
         }}
       >
-        <Grid
-          sx={{
-            gap: 0,
-            gridTemplateColumns: "repeat(3, 1fr)",
-            m: 3,
-            width: "container",
-          }}
-        >
-          {refs.map((ref) => (
-            <Link key={ref._id} to={`/refs/${ref._id}`}>
-              {ref._id}
-            </Link>
-          ))}
-        </Grid>
-      </Flex>
-    </>
+        {refs.map((ref) => (
+          <Link key={ref._id} to={`/refs/${ref._id}`}>
+            {ref._id}
+          </Link>
+        ))}
+      </Grid>
+    </Flex>
   );
 };
 
--- imports/ui/Header.js
+++ imports/ui/Header.js
@@ -1,32 +1,55 @@
-import { Flex, Text, Input } from "theme-ui";
+import { Flex, Text, IconButton, Input } from "theme-ui";
 import { isReady } from "../utils/helpers";
 import AccountToggle from "./AccountToggle";
 import Nav from "./Nav";
 import React from "react";
+import UilPlus from "@iconscout/react-unicons/icons/uil-plus";
+import { controlHeight } from "../utils/theme";
+import { useHistory } from "react-router-dom";
+import { useAccount } from "../utils/hooks";
 
-const Header = () => (
-  <Flex
-    sx={{
-      bg: "background",
-      justifyContent: "space-between",
-      alignItems: "center",
-      position: "sticky",
-      p: 3,
-      top: 0,
-      width: "100wv",
-      zIndex: 1,
-    }}
-  >
-    <Nav />
-    <Flex sx={{ px: 3, width: "ref" }}>
-      <Input
-        type="search"
-        placeholder="Search"
-        sx={{ variant: "input.default" }}
-      />
+const Header = () => {
+  const { userId } = useAccount();
+  const history = useHistory();
+
+  return (
+    <Flex
+      sx={{
+        bg: "background",
+        justifyContent: "space-between",
+        alignItems: "center",
+        position: "sticky",
+        p: 3,
+        top: 0,
+        width: "100wv",
+        zIndex: 1,
+      }}
+    >
+      <Nav />
+      <Flex sx={{ px: 3, width: "ref" }}>
+        <Input
+          type="search"
+          placeholder="Search"
+          sx={{ variant: "input.default" }}
+        />
+      </Flex>
+      <Flex sx={{ width: controlHeight, justifyContent: "flex-end" }}>
+        <IconButton
+          children={<UilPlus />}
+          sx={{ variant: "iconButton.default", mr: 2 }}
+          title="New base ref"
+          onClick={() =>
+            Meteor.call(
+              "refs.insert",
+              { type: "base", parentId: userId },
+              (error, id) => history.push(`/refs/${id}`)
+            )
+          }
+        />
+        <AccountToggle sx={{ minWidth: "auto" }} />
+      </Flex>
     </Flex>
-    <AccountToggle sx={{ ml: 2, minWidth: "auto" }} />
-  </Flex>
-);
+  );
+};
 
 export default Header;
