a97711ae97c4d021b20e3c9f43e42e82168f4326
Andrew Liebchen <liebchen@fb.com>
2021-01-04 21:44:05 -0800

    Cleanup mock view

--- imports/ui/RefMockView.js
+++ imports/ui/RefMockView.js
@@ -1,9 +1,5 @@
-import { isReady } from "../utils/helpers";
-import capitalize from "capitalize";
-import PropTypes from "prop-types";
-import React from "react";
-import { MoreVertical } from "react-feather";
 import PersonCard from "./PersonCard";
+import React from "react";
 
 const imageSize = 120;
 
@@ -14,11 +10,5 @@ const RefMockView = (props) => (
     subtitle="Person"
   />
 );
-
-RefMockView.propTypes = {
-  data: PropTypes.object,
-  showTitle: PropTypes.bool,
-  title: PropTypes.string,
-};
 
 export default RefMockView;
