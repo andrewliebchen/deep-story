6accf22083e8c0f41a9efa37af745f4fa9ee62e6
Andrew Liebchen <liebchen@fb.com>
2021-01-12 19:08:51 -0800

    Cleanup

--- imports/ui/Nav.js
+++ imports/ui/Nav.js
@@ -1,9 +1,9 @@
 import { Box, Flex, Button, Text } from "theme-ui";
 import { Home } from "react-feather";
+import { refTypes } from "../utils/types";
 import { useHistory } from "react-router-dom";
 import React, { useEffect, useRef, useState } from "react";
 import useHover from "@react-hook/hover";
-import { refTypes } from "../utils/types";
 
 const Nav = (props) => {
   const history = useHistory();
@@ -14,27 +14,27 @@ const Nav = (props) => {
     <Box
       ref={target}
       sx={{
-        size: "control",
         position: "relative",
+        size: "control",
       }}
     >
       {isHovering ? (
         <Flex
           sx={{
-            position: "absolute",
-            top: -3,
-            left: -3,
-            width: 300,
-            p: 3,
+            bg: "background",
+            borderRadius: 4,
+            boxShadow: "overlay",
             flexDirection: "column",
             gap: 2,
-            bg: "background",
-            boxShadow: "overlay",
-            borderRadius: 4,
+            left: -3,
+            p: 3,
+            position: "absolute",
+            top: -3,
+            width: 300,
           }}
         >
           <Button
-            variant="button.secondary"
+            sx={{ variant: "button.secondary" }}
             onClick={() => history.push("/refs")}
           >
             <Home />
@@ -44,7 +44,7 @@ const Nav = (props) => {
             const type = refTypes[key];
             return (
               <Button
-                variant="button.secondary"
+                sx={{ variant: "button.secondary" }}
                 key={key}
                 onClick={() => history.push(type.route)}
               >
@@ -56,7 +56,7 @@ const Nav = (props) => {
         </Flex>
       ) : (
         <Button
-          variant="button.secondary"
+          sx={{ variant: "button.secondary" }}
           children={<Home />}
           onClick={() => history.push("/refs")}
         />
